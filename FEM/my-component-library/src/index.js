7e4aee6266c9889ff061bd13e393b9185f0ba6ad
zksmith <zahiersmith96@gmail.com>
2021-02-24 23:34:11 -0500

    application setup

---
+++ FEM/my-component-library/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimaryButton from './components/Buttons';
+
+const App = () => <PrimaryButton>Hello World</PrimaryButton>;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
